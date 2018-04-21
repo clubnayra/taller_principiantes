7521f1a63942730fde89e4b5dfb21807fb01c624
clubnayra <38368138+clubnayra@users.noreply.github.com>
2018-04-21 15:37:00 -0400

    Add files via upload
    2

---
+++ sesion 2/serial_plot/serial_plot.ino
@@ -0,0 +1,42 @@
+/*
+  Analog Input, but with Serial Plotter!
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13.
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead().
+
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ 
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/AnalogInput
+
+ */
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  // begin the serial monitor @ 9600 baud
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+
+  Serial.println(sensorValue);
+  Serial.print(" ");
+ 
+  delay(20);
+}
